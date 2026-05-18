36bac5b89e4db045a795dec1f180d6e95a193062
SunnY <pboonchanachai@gmail.com>
2026-05-18 16:38:07 +0700

    Update base.launch.py

--- ros2_ws/src/f1tenth_bringup/launch/base.launch.py
+++ ros2_ws/src/f1tenth_bringup/launch/base.launch.py
@@ -1,9 +1,8 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, TimerAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch_ros.actions import Node
 
 
 def generate_launch_description():
@@ -26,7 +25,9 @@ def generate_launch_description():
         ),
     )
 
+    delayed_actuator = TimerAction(period=5.0, actions=[actuator_launch])
+
     return LaunchDescription([
-        actuator_launch,
         sensor_launch,
+        delayed_actuator,
     ])
